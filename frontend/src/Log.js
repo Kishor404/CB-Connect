fd2ae0371f208ed44f18f75f3adfbf575ea07c85
kishor400 <kishor404@outlook.in>
2024-05-26 11:41:57 +0530

    Log

---
+++ frontend/src/Log.js
@@ -0,0 +1,16 @@
+import "./Style/Log.css"
+
+function Log(){
+
+    return(
+        <>
+            <section>
+                <div className="Log-Container">
+                    
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Log
